620e8d2d6b978eba82d26ce003332ea8c35c38ee
gurudesh0801 <gurudesh2204@gmail.com>
2025-05-15 00:18:49 +0530

    changes done

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "./i18n.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
